978603a897009e25375b2e81ea4b6a1ccec020c6
Mikail <mikail.yayla@tu-dortmund.de>
2022-04-01 15:36:01 +0200

    restructure and clearn

--- bet_eval/loadData.py
+++ bet_eval/loadData.py
@@ -290,15 +290,51 @@ def readFileWinequality(dataset_path):
     return X, Y
 
 def readFileSpamBase(path):
-	f = open(path, 'r')
-	X = []
-	Y = []
-	for row in f:
-		entries = row.strip().split(",")
-		x = [int(float(e)*100) for e in entries[0:-1]]
+    f = open(path, 'r')
+    X = []
+    Y = []
+    for row in f:
+        entries = row.strip().split(",")
+        x = [int(float(e)*100) for e in entries[0:-1]]
 
-		y = int(entries[-1])
-		X.append(x)
-		Y.append(y)
+        y = int(entries[-1])
+        X.append(x)
+        Y.append(y)
 
-	return np.array(X).astype(dtype=np.int32), np.array(Y)
+    return np.array(X).astype(dtype=np.int32), np.array(Y)
+
+
+def readFileWearable(path):
+    f = open(path, 'r')
+    header = next(f)
+    X = []
+    Y = []
+
+    for row in f:
+        entries = row.replace("\n","").split(";")
+
+        if entries[-1] == 'sitting':
+            y = 0
+        elif entries[-1] == 'standing':
+            y = 1
+        elif entries[-1] == 'standingup':
+            y = 2
+        elif entries[-1] == 'walking':
+            y = 3
+        elif entries[-1] == 'sittingdown':
+            y = 4
+        else:
+            print("ERROR READING CLASSES:", entries[-1])
+
+        x = []
+        if entries[1] == 'Man':
+            x.append(0)
+        else:
+            x.append(1)
+
+        for e in entries[2:-1]:
+            x.append(int(float(e.replace(",","."))*100))
+        X.append(x)
+        Y.append(y)
+
+    return np.array(X).astype(dtype=np.int32), np.array(Y)
--- bet_eval/run_exp.py
+++ bet_eval/run_exp.py
@@ -1,7 +1,5 @@
 # libary imports
 import csv,operator,sys,os
-from sklearn.datasets import load_iris, fetch_olivetti_faces, fetch_covtype
-from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -15,10 +13,10 @@ import os
 import joblib
 
 # own file imports
-from Utils import create_exp_folder, store_exp_data_dict, store_exp_data_write, bit_error_rates_generator, quantize_data
-from loadData import readFileMNIST, readFileAdult, readFileSensorless, readFileWinequality, readFileSpamBase
+from Utils import create_exp_folder, store_exp_data_dict, store_exp_data_write, bit_error_rates_generator
 from pathEvals import tree_nrOfCorrectPredictionsDespiteWrongPath, tree_nrOfChangedPathsWithOneBF, tree_PEs_estim
 from bfi_evaluation import bfi_tree, bfi_forest
+from prepareExpData import getData
 
 def main():
     ### Preparations and configs
@@ -26,7 +24,7 @@ def main():
     this_path = os.getcwd()
 
     # command line arguments, use argparse here later
-    dataset = "SPAMBASE"
+    dataset = "WEARABLE"
 
     # DT/RF configs
     DT_RF = "RF" # DT or RF (needs to be correctly specified when loading a model)
@@ -34,16 +32,17 @@ def main():
     estims = 5 # number of DTs in RF (does not matter for DT)
     split_inj = 1 # activate split value injection with 1
     feature_inj = 0 # activate feature value injection with 1
-    nr_bits_split = None # nr of bits in split value, it is set below when dataset is loaded
-    int_split = 1 # whether to use integer split
-    nr_bits_feature = None # nr of bits in feature value, it is set below when dataset is loaded
+    nr_bits_split = 32 # nr of bits in split value
+    nr_bits_feature = 32 # nr of bits in feature value
+    int_split = 0 # whether to use integer split
     feature_inj = 0 # activate feature value injection with 1
     feature_idx_inj = 0 # activate feature idx injection with 1
     child_idx_inj = 0 # activate child idx injection with 1
     reps = 5 # how many times to evaluate for one bit error rate
     # p2exp = 6 # error rates for evaluation start at 2^(-p2exp)
     # bers = bit_error_rates_generator(p2exp)
-    bers = [0, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+    # bers = [0, 0.0001, 0.001, 0.01, 0.1, 0.2, 0.4, 0.6, 0.8, 1]
+    bers = [0, 0.0001, 0.001]
     export_accuracy = 1 # 1 if accuracy list for a bit error rate should be exported as .npy, else None
     all_data = []
     random_state = 42 #np.random.randint(low=1, high=100)
@@ -56,97 +55,7 @@ def main():
     true_majority = 1
 
     # read data
-    train_path = ""
-    test_path = ""
-    X_train, y_train, X_test, y_test = None, None, None, None
-
-    if dataset == "MNIST":
-        nr_bits_split = 8
-        nr_bits_feature = 8
-        dataset_train_path = "/mnist/dataset/train.csv"
-        dataset_test_path = "/mnist/dataset/test.csv"
-        train_path = this_path + dataset_train_path
-        test_path = this_path + dataset_test_path
-        X_train, y_train = readFileMNIST(train_path)
-        X_test, y_test = readFileMNIST(test_path)
-
-    if dataset == "IRIS":
-        nr_bits_split = 7
-        nr_bits_feature = 7
-        dataset_train_path = "/sklearn"
-        dataset_test_path = "/sklearn"
-        train_path = this_path + dataset_train_path
-        test_path = this_path + dataset_test_path
-        iris = load_iris()
-        X, y = iris.data, iris.target
-        X *= 10
-        X = X.astype(np.uint8)
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=random_state)
-
-    if dataset == "ADULT":
-        nr_bits_split = 8 # int, use 32 for fp
-        nr_bits_feature = 8 # int, use 32 for fp
-        dataset_path = "adult/dataset/adult.data"
-        X, y = readFileAdult(dataset_path)
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=random_state)
-        # comment out quantization, if it is not desired
-        X_train = quantize_data(X_train, nr_bits_feature)
-        X_test = quantize_data(X_test, nr_bits_feature)
-
-    if dataset == "SENSORLESS":
-        nr_bits_split = 32 # floating point
-        nr_bits_feature = 32 # floating point
-        dataset_path = "sensorless-drive/dataset/Sensorless_drive_diagnosis.txt"
-        X, y = readFileSensorless(dataset_path)
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=random_state)
-
-    if dataset == "WINEQUALITY":
-        nr_bits_split = 32 # floating point
-        nr_bits_feature = 32 # floating point
-        dataset_path = "wine-quality/dataset/"
-        X, y = readFileWinequality(dataset_path)
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=random_state)
-
-    if dataset == "OLIVETTI":
-        nr_bits_split = 8
-        nr_bits_feature = 8
-        dataset_train_path = "/sklearn"
-        dataset_test_path = "/sklearn"
-        train_path = this_path + dataset_train_path
-        test_path = this_path + dataset_test_path
-        X, y = fetch_olivetti_faces(shuffle=True, random_state=random_state, download_if_missing=True, return_X_y=True)
-        X = np.array(X*255).astype(np.uint8) # use unsigned ints
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=random_state)
-
-    if dataset == "COVTYPE":
-        nr_bits_split = 8
-        nr_bits_feature = 8
-        dataset_train_path = "/sklearn"
-        dataset_test_path = "/sklearn"
-        train_path = this_path + dataset_train_path
-        test_path = this_path + dataset_test_path
-        X, y = fetch_covtype("experiments/COVTYPE/", shuffle=True, random_state=random_state, download_if_missing=True, return_X_y=True)
-        X = np.array(X).astype(np.uint8) # use unsigned ints
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=random_state)
-
-    if dataset == "SPAMBASE":
-        nr_bits_split = 8
-        nr_bits_feature = 8
-        dataset_path = "spambase/dataset/spambase.data"
-        X, y = readFileSpamBase(dataset_path)
-        # rint = np.random.randint(low=1, high=100)
-        X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.33, random_state=random_state)
-
+    X_train, y_train, X_test, y_test = getData(dataset, this_path, nr_bits_split, nr_bits_feature, random_state)
 
     # create experiment folder and return the path to it
     exp_path = create_exp_folder(this_path)
@@ -170,8 +79,6 @@ def main():
 
     # create data file to store experiment results
     exp_data = open(exp_path + "/results.txt", "a")
-    exp_data.write(train_path+"\n")
-    exp_data.write(test_path+"\n")
     exp_data.close()
 
     # dictionary for experiment data
